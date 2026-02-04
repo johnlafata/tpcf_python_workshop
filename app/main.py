d96a774f4195319f672ad536a5d97b21d38f1fd8
John LaFata <johnlafata61@gmail.com>
2026-02-04 10:55:55 -0500

    update greeting

--- app/main.py
+++ app/main.py
@@ -13,7 +13,7 @@ books = [
 
 @app.get("/")
 def read_root():
-    return {"message": "Welcome to the this updated Amazing Books API"}
+    return {"message": "Welcome to this updated Amazing Books API"}
 
 @app.get("/books", response_model=List[Book])
 def get_books():
